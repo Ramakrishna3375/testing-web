11c364741161f047068e4779c7da719904064937
SrinivasCresol <srinivas.y@cresolinfoserv.com>
2025-08-29 19:55:43 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import {Route ,Routes, Navigate} from 'react-router-dom';
-import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Components/HomePage.jsx";
+import MobilesPage from "./Components/Mobiles/AdsPage.jsx";
+import ProductDetailPage from "./Components/Mobiles/AdDetailsPage.jsx";
+import PostFreeAdPage from "./Components/PostFreeAd/PostFreeAdPage.jsx";
+import PostAdForm from "./Components/PostFreeAd/PostAdForm.jsx";
 
-import HomePage from './Components/HomePage.jsx';
-import MobilesPage from './Components/Mobiles/AdsPage.jsx';
-import ProductDetailPage from './Components/Mobiles/AdDetailsPage.jsx';
-import PostFreeAdPage from './Components/PostFreeAd/PostFreeAdPage.jsx'
-import PostAdForm from './Components/PostFreeAd/PostAdForm.jsx';
-import Json from './Json.jsx'
-
-function App(){
-return(
+function App() {
+  return (
     <>
-    <Routes>
+      <Routes>
         {/* Default route */}
         <Route path="/" element={<Navigate to="/homepage" replace />} />
-
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/ads/:categoryId" element={<MobilesPage />} />
         <Route path="/ad/:id" element={<ProductDetailPage />} />
         <Route path="/post-free-ad" element={<PostFreeAdPage />} />
-        <Route path="/post-free-ad/:category/:subcategory" element={<PostAdForm />} />
-        <Route path="/json" element={<Json />} />
-    </Routes>
+        <Route
+          path="/post-free-ad/:category/:subcategory"
+          element={<PostAdForm />}
+        />
+      </Routes>
     </>
-);
+  );
 }
 export default App;
